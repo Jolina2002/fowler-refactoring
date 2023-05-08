61f6e9e04d1ce786a54ecc03c592c6529da58070
jolinaheesen <jolina.heesen@gmail.com>
2023-05-08 20:49:25 +0200

    Added Inheritance

--- src/Rental.java
+++ src/Rental.java
@@ -14,7 +14,10 @@ class Rental {
         return movie;
     }
     int getFrequentRenterPoints() {
-        return movie.getFrequentRenterPoints(daysRented);
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
     }
 
     double getCharge() {
